553dbcac3d8bddbda9c642817efff325f8d71fcf
Ledwos <32643853+Ledwos@users.noreply.github.com>
2020-07-14 10:48:41 +0100

    db w/ custom model

--- newsquest/game/models.py
+++ newsquest/game/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
+
 # Create your models here.
+class CustomUser(AbstractUser):
+    username = None
+    first_name = None
+    last_name = None
+    email = models.EmailField(_('email address'), unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    points = models.IntegerField(default=0, verbose_name=0)
+    # don't see the need for level just yet
+    # level = models.IntegerField(default=0, verbose_name=0)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
--- newsquest/game/views.py
+++ newsquest/game/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse as hr
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 # Create your views here.
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
